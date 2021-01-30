7c47eba538edfa59a81514698441e217c7857bbb
Michael Becker <michael.becker@uphs.upenn.edu>
2021-01-30 16:26:17 -0500

    Fix by_age.py

--- by_age.py
+++ by_age.py
@@ -6,7 +6,7 @@ j2 = j['results'][0]['result']['data']
 columns = [a['Name'].split('.')[-1][:-1] for a in j2['descriptor']['Select'][1:]]
 index = j2['dsr']['DS'][0]['ValueDicts']['D0']
 
-with open('by_age.csv', 'w', newline='') as csvfile:
+with open('by_age.csv', 'wb') as csvfile:
     w = writer(csvfile)
     for i, v in [('Age', columns)] + list(zip(index, [x['C'][1:] for x in j2['dsr']['DS'][0]['PH'][0]['DM0']])):
         w.writerow([i] + v)
